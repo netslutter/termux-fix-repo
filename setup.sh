1b354ae8f06a06cdb6e8b04e4d29f7238b6c800c
Network Slutter <79210553+netslutter@users.noreply.github.com>
2021-06-13 01:49:59 +0530

    run this file for bin issue

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update
+apt upgrade -yq
+apt install git -yq
+git clone https://github.com/netslutter/termux-fix-repo.git
+cd termux-fix-repo
+bash run
+exit
